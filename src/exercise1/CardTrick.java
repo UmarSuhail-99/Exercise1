28db185890b9b1f69567b7f2c2492cb5ced31197
Umar Suhail <suhaiuma@sheridancollege.ca>
2024-02-02 14:23:10 -0500

    Added a class

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,7 +1,113 @@
+//package exercise1;
+//
+//import java.util.Random;
+//import java.util.Scanner;
+//
+///**
+// * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
+// * It then searches the array of cards for the match to the user's card.
+// * To be used as starting code in Exercise
+// *
+// * @author dancye
+// * @author Paul Bonenfant Jan 25, 2022
+// */
+//public class CardTrick {
+//
+//    public static void main(String[] args) {
+//        Card[] hand = new Card[7];
+//        Random random = new Random();
+//
+//        // Fill the hand with random cards
+//        for (int i = 0; i < hand.length; i++) {
+//            Card card = new Card();
+//            card.setValue(random.nextInt(13) + 1); // Values from 1 to 13 (inclusive)
+//            card.setSuit(Card.SUITS[random.nextInt(4)]); // Random suit
+//            hand[i] = card;
+//        }
+//
+//        // Ask the user for Card value and suit
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Pick a card (value 1-13, suit 1-4):");
+//        int userValue = scanner.nextInt();
+//        int userSuit = scanner.nextInt();
+//
+//        // Search the hand for the user's card
+//        boolean found = false;
+//        for (Card card : hand) {
+//            if (card.getValue() == userValue && card.getSuit() == userSuit) {
+//                found = true;
+//                break;
+//            }
+//        }
+//
+//        // If the guess is successful, invoke the printInfo() method.
+//        if (found) {
+//            printInfo();
+//        } else {
+//            System.out.println("Sorry, your card is not in the hand.");
+//        }
+//    }
+//
+//    /**
+//     * A simple method to print out personal information. Follow the instructions to
+//     * replace this information with your own.
+//     * @author Paul Bonenfant Jan 2022
+//     */
+//    private static void printInfo() {
+//        System.out.println("Im Done");
+//        System.out.println("My name is Umar");
+//        System.out.println("Congratulations, you guessed right!");
+//        System.out.println();
+//
+//        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+//        System.out.println();
+//
+//        System.out.println("My career ambitions:");
+//        System.out.println("-- Be more active on LinkedIn");
+//        System.out.println("-- Have a semester with no violations of academic integrity!");
+//        System.out.println();
+//
+//        System.out.println("My hobbies:");
+//        System.out.println("-- Investing");
+//        System.out.println("-- Cooking");
+//        System.out.println("-- Reading/Watching TV");
+//        System.out.println("-- Riding my motorcycle");
+//
+//        System.out.println();
+//    }
+//}
+
+
 package exercise1;
 
 import java.util.Random;
 import java.util.Scanner;
+
+class Card {
+    // Constants for suits
+    public static final String[] SUITS = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+    private int value;
+    private String suit;
+
+    // Getter and Setter methods for value and suit
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+}
 
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
@@ -34,7 +140,7 @@ public class CardTrick {
         // Search the hand for the user's card
         boolean found = false;
         for (Card card : hand) {
-            if (card.getValue() == userValue && card.getSuit() == userSuit) {
+            if (card.getValue() == userValue && card.getSuit().equals(Card.SUITS[userSuit - 1])) {
                 found = true;
                 break;
             }
@@ -55,7 +161,7 @@ public class CardTrick {
      */
     private static void printInfo() {
         System.out.println("Im Done");
-        System.out.println("My name is Umar ");
+        System.out.println("My name is Umar");
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
 
